15d2563a479046a03d9c29b714e66736253c89cd
tanguybernard <bernardtanguy1pro@gmail.com>
2021-07-22 15:27:43 +0200

    refacto

--- src/main/kotlin/domain/Game.kt
+++ src/main/kotlin/domain/Game.kt
@@ -15,13 +15,17 @@ class Game {
 
     fun printScore(): String {
         fromAdvantageToDeuce()
-        if (playerOneScore == 5 || (playerOneScore == 4 && playerTwoScore < 3)) return "player one wins"
-        if (playerTwoScore == 5 ||  (playerTwoScore == 4 && playerOneScore < 3)) return "player two wins"
+        if (didPlayerOneWin()) return "player one wins"
+        if (didPlayerTwoWin()) return "player two wins"
         if (playerOneScore == 4) return "advantage player one"
         if (playerTwoScore == 4) return "advantage player two"
         return if (playerOneScore == 3 && playerTwoScore == 3) "deuce"
         else "${printPlayerScore(playerOneScore)}-${printPlayerScore(playerTwoScore)}"
     }
+
+    private fun didPlayerTwoWin(): Boolean = playerTwoScore == 5 || (playerTwoScore == 4 && playerOneScore < 3)
+
+    private fun didPlayerOneWin(): Boolean = playerOneScore == 5 || (playerOneScore == 4 && playerTwoScore < 3)
 
     private fun fromAdvantageToDeuce() {
         if (playerOneScore == 4 && playerTwoScore == 4) {
